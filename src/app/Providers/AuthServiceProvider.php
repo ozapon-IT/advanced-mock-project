c76df1036a50fc9d99b582cd460eb0bd4bed8d70
ozawa takashi <ozapon4@gmail.com>
2025-02-26 23:06:44 +0900

    メール認証カスタマイズ

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail as VerifyEmailNotification;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmailNotification::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('【Rese】メール認証のお願い')
+                ->greeting('こんにちは、' . $notifiable->name . 'さん!')
+                ->line('アカウントのセキュリティーを確保するために、メールアドレスの認証を完了してください。')
+                ->action('メール認証を完了する', $url)
+                ->line('このメールに心当たりがない場合は、このまま無視してください。');
+        });
     }
 }
